8008c099f4221d415c73552dea56b0d31160b78f
zhangzj-b <zhangzj-b@glodon.com>
2016-05-18 16:48:12 +0800

    菜单优化

--- webApp/services/views/auth/member/services.member.nav.js
+++ webApp/services/views/auth/member/services.member.nav.js
@@ -20,23 +20,70 @@ App.Services.MemberNav=Backbone.View.extend({
     },
     //外部用户
     outer:function(){
+        var _this =this;
         App.Services.MemberType = "outer";
         $(".serviceBody .content").addClass("services_loading");
-        this.init();
+        App.Services.queue.promise(_this.pull,_this);
     },
     //内部用户
     inner:function(){
+        var _this =this;
         App.Services.MemberType = "inner";
         $(".serviceBody .content").addClass("services_loading");
-        this.init();
+        App.Services.queue.promise(_this.pull,_this);
     },
     //加载子组织，刷新右侧组织和员工列表
-    init:function(){
+    pull:function(){
         var _thisType = App.Services.MemberType,
-            _this =this,
+            cdata,
+            _this = App.Services.queue.present[0],
             collection = App.Services.Member[_thisType + "Collection"];
+
+
+        $("#blendList").empty();
+
+
+        cdata = {
+            URLtype: "fetchServicesMemberList",
+            type:"GET",
+            data:{
+                outer:  !(_thisType == "inner"),
+                includeUsers:true
+            }
+        };
+
+        App.Comm.ajax(cdata,function(response){
+
+            //样式处理
+            _this.$("div").removeClass("active");
+            $("#" + _thisType).addClass("active");
+            $(".serviceOgList span").removeClass("active");//唯一选项
+            $("#" + _thisType + " > span").addClass("active");//选中状态
+            $(".serviceBody .content").removeClass("services_loading");
+            if(!response.data.org.length && !response.data.user.length ){
+                $("#blendList").html("<li><span class='sele'>暂无数据</span></li>");
+                return
+            }
+
+            collection.reset();
+            if(response.data.user && response.data.user.length){
+                collection.add(response.data.user);
+            }
+            if (response.data.org && response.data.org.length) {
+                _this.$(".childOz").empty();
+                collection.add(response.data.org);
+                //外部和内部单选
+
+                //菜单渲染
+                $("#" + _thisType +"+ .childOz").html(App.Services.tree(response));
+            }
+        }).done(function(){
+            App.Services.queue.next();
+        });
+
+
         //获取数据，将会刷新右侧视图
-        App.Services.Member.loadData(collection,{},function(response){
+        /*App.Services.Member.loadData(collection,{},function(response){
             //菜单
             if (response.data.org && response.data.org.length) {
                 //样式处理
@@ -53,6 +100,6 @@ App.Services.MemberNav=Backbone.View.extend({
                 $("#blendList").html("<li>&nbsp;&nbsp;&nbsp;&nbsp;暂无数据!</li>");
             }
             $(".serviceBody .content").removeClass("services_loading");
-        });
+        });*/
     }
 });
--- webApp/services/views/auth/member/services.member.ozdetail.js
+++ webApp/services/views/auth/member/services.member.ozdetail.js
@@ -27,37 +27,16 @@ App.Services.MemberozDetail=Backbone.View.extend({
     },
 
     unfold:function(){
-        var _this = this;
-
+        var _this =  this;
         //如果是已有元素，并快速点击，属于误操作，跳过
         if(this.$(".ozName span").hasClass("active") && !App.Services.queue.permit){return;}
-
-        //队列机制，尚未完成
-        if(!App.Services.queue.permit){
-            if(App.Services.queue.que.length > 1){
-                App.Services.queue.que.splice(1);
-            }
-            App.Services.queue.que.push(this.promise);
-            return
-        }
-
         //许可证相关
-        App.Services.queue.permit = false;
-        this.permit(); //200ms后发放一个许可证，避免点击过快
+        App.Services.queue.promise(_this.pull,_this);
 
-        //样式操作
-        $(".serviceOgList span").removeClass("active");
-        _this.$(".ozName > span").addClass("active");
-        $("#blendList").empty();//刷新右侧数据
-        $(".serviceBody .content").addClass("services_loading");
-        
-
-        this.promise();
         //获取数据，将会刷新右侧视图
         /*App.Services.Member.loadData(collection,data,function(response){
             //菜单
             if (response.data.org && response.data.org.length) {
-
                 //菜单渲染
                 $("#childOz" + _this.model.cid).html(App.Services.tree(response));
             }
@@ -69,12 +48,19 @@ App.Services.MemberozDetail=Backbone.View.extend({
     },
 
     //队列请求
-    promise:function(){
-        var _this = this;
-        var _thisType = App.Services.MemberType;
-        var _thisId = App.Services.memFatherId =  this.$(".ozName").data("id") ;
+    pull:function(){
 
+        var _this = App.Services.queue.present[0];
+        var _thisType = App.Services.MemberType;
+        var _thisId = App.Services.memFatherId =  _this.$(".ozName").data("id") ;
         var collection = App.Services.Member[_thisType + "Collection"];
+
+
+        //样式操作
+        $(".serviceOgList span").removeClass("active");
+        _this.$(".ozName > span").addClass("active"); //this偏差导致选择不正确
+        $("#blendList").empty();
+        $(".serviceBody .content").addClass("services_loading");
         var cdata = {
             URLtype: "fetchServicesMemberList",
             type:"GET",
@@ -84,12 +70,11 @@ App.Services.MemberozDetail=Backbone.View.extend({
                 includeUsers:true
             }
         };
-
         //此处为延迟
         App.Comm.ajax(cdata,function(response){
+            $(".serviceBody .content").removeClass("services_loading");
             if(!response.data.org.length && !response.data.user.length ){
                 $("#blendList").html("<li><span class='sele'>暂无数据</span></li>");
-                $(".serviceBody .content").removeClass("services_loading");
                 return
             }
             collection.reset();
@@ -101,20 +86,10 @@ App.Services.MemberozDetail=Backbone.View.extend({
                 //菜单渲染
                 $("#childOz" + _this.model.cid).html(App.Services.tree(response));
             }
-            $(".serviceBody .content").removeClass("services_loading");
         }).done(function(){
-            //添加执行队列
-             App.Services.queue.que.splice(0,0);
-            if(App.Services.queue.que.length){
-                App.Services.queue.que[0]();
-            }
+            //删除执行完毕的 ，添加执行新的
+             App.Services.queue.next();
         });
-    },
-
-    permit:function(){
-        setTimeout(function(){
-            App.Services.queue.permit = true;
-        },1000);
     }
 });
 
