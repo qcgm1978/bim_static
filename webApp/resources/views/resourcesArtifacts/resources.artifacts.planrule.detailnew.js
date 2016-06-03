493612c2d5db6fb08c739f62971234dece8c506f
zhangzj-b <zhangzj-b@glodon.com>
2016-06-03 16:29:30 +0800

    1

--- webApp/resources/views/resourcesArtifacts/resources.artifacts.planrule.detailnew.js
+++ webApp/resources/views/resourcesArtifacts/resources.artifacts.planrule.detailnew.js
@@ -8,7 +8,9 @@ App.Resources.ArtifactsPlanRuleDetailNew = Backbone.View.extend({
     template: _.templateUrl("/resources/tpls/resourcesArtifacts/resources.artifacts.planruledetailnew.html"),
 
     events:{
-        "click .delRule": "delRule"
+        "click .delRule": "delRule",
+        "click .myItem":"myItem",
+        "click .myDropText":"seleRule",
     },
 
     render:function() {
@@ -18,6 +20,34 @@ App.Resources.ArtifactsPlanRuleDetailNew = Backbone.View.extend({
 
     initialize:function(){
         this.listenTo(this.model,"change",this.render);
+    },
+
+    //切换规则
+    seleRule:function(e){
+        $(".myDropList").hide();
+        var _this = $(e.target);
+        _this.siblings(".myDropList").show();
+    },
+
+    //选择规则，切换输入方式
+    myItem:function(e){
+        var _this = $(e.target);
+        var operator = _this.data("operator");
+        var text = _this.text();
+        var parent =  _this.parent(".myDropList");
+        var eIn = _this.closest(".leftten").siblings(".eIn");
+        var ioside  = _this.closest(".leftten").siblings(".ioside");
+        //数据写入模型
+        parent.hide().siblings(".myDropText").find(".text").text(text);
+        if(operator == "==" || operator == "!="){
+            ioside.removeClass("active");
+            if(eIn.hasClass("active")){return}
+            eIn.addClass("active");
+        }else if(operator == "<>" || operator == "><"){
+            eIn.removeClass("active");
+            if(ioside.hasClass("active")){return}
+            ioside.addClass("active");
+        }
     },
 
     //删除
