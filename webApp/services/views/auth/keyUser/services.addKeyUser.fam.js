2a4d3b903be50b68d1142cbfc76ccef9da82b691
zen <five44@163.com>
2016-06-07 10:58:25 +0800

    no message

---
+++ webApp/services/views/auth/keyUser/services.addKeyUser.fam.js
@@ -0,0 +1,41 @@
+
+var App = App || {};
+App.Services.fam = Backbone.View.extend({
+
+  tagName:'div',
+
+  className:'fam',
+
+  template:_.templateUrl("/services/tpls/auth/keyUser/services.addKeyUser.fam.html"),
+
+  events:{
+    "click li":"changeStatus"
+  },
+
+  render:function(){
+
+      var datas={
+        direction : App.Services.KeyUser.fam.toJSON() || []
+
+      };
+    console.log(datas)
+      this.$el.html(this.template(datas));
+
+
+    return this;
+  },
+
+
+  initialize:function(){
+    this.listenTo(App.Services.KeyUser.fam,'add',this.render)
+  },
+
+  //选定族库
+  changeStatus:function(e){
+
+    var $li=$(e.currentTarget);
+
+    $li.toggleClass('selected-proj');
+
+  }
+});
