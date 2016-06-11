af0aa1577c2474eaea545069ca5d85eabfe8cf29
zhangzj-b <zhangzj-b@glodon.com>
2016-06-11 13:00:50 +0800

    2

--- webApp/resources/views/resourcesArtifacts/ruleModel/resources.artifacts.plandetail.js
+++ webApp/resources/views/resourcesArtifacts/ruleModel/resources.artifacts.plandetail.js
@@ -24,13 +24,14 @@ App.Resources.ArtifactsPlanDetail = Backbone.View.extend({
 
     changeCount:function(){
         var count = App.ResourceArtifacts.Status.rule.count;
-        console.log(this.model);
         this.model.set({count:count},{silent:true});
         this.$(".count").text("("+ count + ")");
     },
 
     //取得规则列表
     getPlanId:function(e){
+
+
         var $this = $(e.target);
         if($this.closest("li").hasClass("active")){
             return
@@ -45,6 +46,7 @@ App.Resources.ArtifactsPlanDetail = Backbone.View.extend({
             return
         }
         App.ResourceArtifacts.loading($(".rules"));
+
         this.toggleClass();
         this. getRules();
 
@@ -64,7 +66,7 @@ App.Resources.ArtifactsPlanDetail = Backbone.View.extend({
             URLtype: "fetchArtifactsPlanRule",
             data:{
                 code:App.ResourceArtifacts.Status.rule.targetCode,
-                biz :App.ResourceArtifacts.Status.rule.biz,
+                biz : 1,
                 type:App.ResourceArtifacts.Status.type,
                 projectId:App.ResourceArtifacts.Status.projectId
             }
@@ -72,16 +74,12 @@ App.Resources.ArtifactsPlanDetail = Backbone.View.extend({
         App.ResourceArtifacts.loading();
         App.Comm.ajax(pdata,function(response){
             if(response.code == 0 ){
-
-                /*$(".artifactsContent .rules h2 .name").html(_this.model.get("code") + "&nbsp;" +_this.model.get("name"));
-                $(".artifactsContent .rules h2 i").html( "("+response.data.length + ")");*/
-
                 if(response.data  &&  response.data.length){
                     App.ResourceArtifacts.PlanRules.reset();
                     App.ResourceArtifacts.PlanRules.add(response.data);
                     Backbone.trigger("resetTitle");
                 }else{
-                    $(".ruleContent ul").html("<li><div class='ruleTitle delt'>暂无内容</div></li>");
+                    $(".ruleContentRuleList ul").html("<li><div class='ruleTitle delt'>暂无内容</div></li>");
                 }
             }
             App.ResourceArtifacts.loaded($(".rules"));
