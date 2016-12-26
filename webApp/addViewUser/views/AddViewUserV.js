bda66b08fc46d64555093d1c9238cd29ee37ee96
zhangyk-a <zhangyk-a@ZHANGYK-A-PC>
2016-12-26 18:11:35 +0800

    更新了用户管理的功能代码

--- webApp/addViewUser/views/AddViewUserV.js
+++ webApp/addViewUser/views/AddViewUserV.js
@@ -1,10 +1,26 @@
 /*!/addViewUser/collection/index.js调用*/
 App.AddViewUser.AddViewUserV = Backbone.View.extend({
+	default:{
+		state:{
+			selectedIndex:"userList",
+			showIndex:"userList"
+		}
+	},
 	el:$("#contains"),
 	template:_.templateUrl("/addViewUser/tpls/index.html"),
+	events: {
+ 		"click .viewUserTopTab li": "switchTab",
+ 	},
 	render:function(){
-	    this.$el.html(this.template);
+	    this.$el.html(this.template({state:this.default.state}));
 	    return this;
 	},
-
+	switchTab:function(event){
+		var target = $(event.target);
+		if(!target.hasClass('selected')){
+			this.default.state.selectedIndex = target.data('type');
+			this.default.state.showIndex = target.data('type');
+			this.render();
+		}
+	}
 });
