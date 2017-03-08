74ccc71684c92524350b24ad2f29349d5049f4a6
zhangyk-a <zhangyk-a@ZHANGYK-A-PC>
2017-03-08 16:27:39 +0800

    修改了一些问题

--- webApp/backStage/collection/setPermissionsC.js
+++
@@ -1,7 +0,0 @@
-App.setPermissions = {
-	init:function(){
-		$("#contains").empty();
-		var SetPermissionsIndexV = new App.setPermissions.SetPermissionsIndexV(); //渲染框架
-		SetPermissionsIndexV.render();
-	},
-}
--- webApp/backStage/view/backStageV.js
+++
@@ -1,8 +0,0 @@
-App.backStage.BackStageIndexV = Backbone.View.extend({
-	el:$("#contains"),
-	template:_.templateUrl("/backStage/tpls/index.html"),
-	render:function(){
-		this.$el.html(this.template());
-		return this;
-	},
-})
